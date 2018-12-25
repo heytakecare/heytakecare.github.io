74f1f7715efe6ac39b37156ca18608c5e82b256f
mathewtrivett <matt.trivett@gmail.com>
2018-12-25 17:24:13 +0000

    build

--- js/app.js
+++ js/app.js
@@ -2,29 +2,27 @@
 
 var placedImages = [];
 var imageContainer = document.getElementById('images');
-var images = JSON.parse(imageContainer.dataset.images);
 
-imageContainer.onclick = function(event) {
-  let imgData = {
-    "xPos": event.clientX,
-    "yPos": event.clientY,
-    "src": randomChoice(images)
+if (imageContainer != undefined) {
+  var images = JSON.parse(imageContainer.dataset.images);
+  imageContainer.onclick = function(event) {
+    let imgData = {
+      "xPos": event.clientX,
+      "yPos": event.clientY,
+      "src": randomChoice(images)
+    }
+    placedImages.push(imgData);
+    var img = document.createElement('img');
+    img.src = imgData.src;
+    img.style.position = "absolute";
+    img.style.left = imgData.xPos + "px";
+    img.style.top = imgData.yPos + "px";
+    img.classList.add('placed');
+    this.appendChild(img);
   }
-  placedImages.push(imgData);
-  var img = document.createElement('img');
-  img.src = imgData.src;
-  img.style.position = "absolute";
-  img.style.left = imgData.xPos + "px";
-  img.style.top = imgData.yPos + "px";
-  img.classList.add('placed');
-  this.appendChild(img);
 }
 
 function randomChoice(choices) {
   let index = Math.floor(Math.random() * choices.length);
   return choices[index];
-}
-
-function toggleMenu() {
-  
 }
